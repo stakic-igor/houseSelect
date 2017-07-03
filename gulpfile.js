37b77300fe852f6239a5fb9d8e17afa46b556f74
stakic-igor <stakic_igor@yahoo.com>
2017-07-03 22:22:40 +0200

    preheader

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+//register components
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer');
+
+//define plugins
+gulp.task('sass', () => {
+  return gulp.src('scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: ['node_modules/susy/sass'] //compile susy
+    }).on('error', sass.logError))
+    .pipe(sourcemaps.write('maps/'))
+    .pipe(gulp.dest('css/'))
+});
+
+gulp.task('autoprefixer', () => {
+  return gulp.src('css/base.css')
+   .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('css/'))
+});
+
+//define watch task
+gulp.task('watch', () => {
+  gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch('./css/base.css', ['autoprefixer']);
+});
